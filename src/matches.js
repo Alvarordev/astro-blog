f82a191458cc7a2f2a2b7282efd459d2c915e809
Alvaro <alvarord519@gmail.com>
2023-08-16 18:36:36 -0500

    Post components

--- src/matches.js
+++ src/matches.js
@@ -3,35 +3,35 @@ const posts = [
       slug: "nottingham-vs-sheffield",
       title: "Nottingham vs Sheffield",
       description: "Coming into The Premier League Gameweek 2 we analize the match between the sides of Nottingham Forest and Sheffield United, who will play their first match since the season 2019/20 at the championship",
-      url: "./images/Forest-vs-Shefield.jpg",
+      url: "https://res.cloudinary.com/dm5qidcka/image/upload/v1692228831/lr5rhrawylaydctgwsib.jpg",
       date: "2023-08-15"
     },
     {
       slug: "wolves-vs-brighton",
       title: "Wolverhampton vs Brighton",
       description: "Coming into The Premier League Gameweek 2 we analize the match between the sides of Nottingham Forest and Sheffield United, who will play their first match since the season 2019/20 at the championship",
-      url: "./images/wolves-vs-brighton.jpg",
+      url: "https://res.cloudinary.com/dm5qidcka/image/upload/v1692228831/c83nmkckfde6jjpmky5i.jpg",
       date: "2023-08-15"
     },
     {
       slug: "liverpool-vs-boutnmounth",
       title: "Liverpool vs Bournmounth",
       description: "Coming into The Premier League Gameweek 2 we analize the match between the sides of Nottingham Forest and Sheffield United, who will play their first match since the season 2019/20 at the championship",
-      url: "./images/liverpool-vs-bournmounth.avif",
+      url: "https://res.cloudinary.com/dm5qidcka/image/upload/v1692228830/ulwpfcuh4wxyjwvkmi6i.avif",
       date: "2023-08-15"
     },
     {
       slug: "fullham-vs-brentford",
       title: "Fullham vs Brentford",
       description: "Coming into The Premier League Gameweek 2 we analize the match between the sides of Nottingham Forest and Sheffield United, who will play their first match since the season 2019/20 at the championship",
-      url: "./images/fullham-vs-brentford.jpg",
+      url: "https://res.cloudinary.com/dm5qidcka/image/upload/v1692228831/whzogsao1rormhw96ute.jpg",
       date: "2023-08-15"
     },
     {
       slug: "totthenham-vs-man-united",
       title: "Tottenham vs Man United",
       description: "Coming into The Premier League Gameweek 2 we analize the match between the sides of Nottingham Forest and Sheffield United, who will play their first match since the season 2019/20 at the championship",
-      url: "./images/tottenham-vs-manunited.jpg",
+      url: "https://res.cloudinary.com/dm5qidcka/image/upload/v1692228858/r1fsxkzbcrvupbabaycq.jpg",
       date: "2023-08-15"
     }
   
