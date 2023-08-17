2336bdd7b69665c9ed6aaafd58ae5dc388384d19
Alvaro <alvarord519@gmail.com>
2023-08-16 20:24:10 -0500

    post changes

--- src/matches.js
+++ src/matches.js
@@ -2,7 +2,10 @@ const posts = [
     {
       slug: "nottingham-vs-sheffield",
       title: "Nottingham vs Sheffield",
-      description: "Coming into The Premier League Gameweek 2 we analize the match between the sides of Nottingham Forest and Sheffield United, who will play their first match since the season 2019/20 at the championship",
+      description: `Nottingham Forest recibe a Sheffield United al City Ground este viernes por la segunda fecha de la Premier League 2023/24, ambos conjuntos vienen de perder en la primera jornada y buscarán su primera victoria en el torneo.
+      
+      Los locales vienen de caer por 2-1 frente al Arsenal, mientras que los 'Blades' perdieron por 1-0 frente al Cristal Palace.`,
+      preview: '',
       url: "https://res.cloudinary.com/dm5qidcka/image/upload/v1692228831/lr5rhrawylaydctgwsib.jpg",
       date: "2023-08-15"
     },
@@ -10,6 +13,7 @@ const posts = [
       slug: "wolves-vs-brighton",
       title: "Wolverhampton vs Brighton",
       description: "Coming into The Premier League Gameweek 2 we analize the match between the sides of Nottingham Forest and Sheffield United, who will play their first match since the season 2019/20 at the championship",
+      preview: '',
       url: "https://res.cloudinary.com/dm5qidcka/image/upload/v1692228831/c83nmkckfde6jjpmky5i.jpg",
       date: "2023-08-15"
     },
@@ -17,6 +21,7 @@ const posts = [
       slug: "liverpool-vs-boutnmounth",
       title: "Liverpool vs Bournmounth",
       description: "Coming into The Premier League Gameweek 2 we analize the match between the sides of Nottingham Forest and Sheffield United, who will play their first match since the season 2019/20 at the championship",
+      preview: '',
       url: "https://res.cloudinary.com/dm5qidcka/image/upload/v1692228830/ulwpfcuh4wxyjwvkmi6i.avif",
       date: "2023-08-15"
     },
@@ -24,6 +29,7 @@ const posts = [
       slug: "fullham-vs-brentford",
       title: "Fullham vs Brentford",
       description: "Coming into The Premier League Gameweek 2 we analize the match between the sides of Nottingham Forest and Sheffield United, who will play their first match since the season 2019/20 at the championship",
+      preview: '',
       url: "https://res.cloudinary.com/dm5qidcka/image/upload/v1692228831/whzogsao1rormhw96ute.jpg",
       date: "2023-08-15"
     },
@@ -31,6 +37,7 @@ const posts = [
       slug: "totthenham-vs-man-united",
       title: "Tottenham vs Man United",
       description: "Coming into The Premier League Gameweek 2 we analize the match between the sides of Nottingham Forest and Sheffield United, who will play their first match since the season 2019/20 at the championship",
+      preview: '',
       url: "https://res.cloudinary.com/dm5qidcka/image/upload/v1692228858/r1fsxkzbcrvupbabaycq.jpg",
       date: "2023-08-15"
     }
